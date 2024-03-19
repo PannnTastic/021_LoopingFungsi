0234b54f5495f588aece2da8d0c9f0ab1a45af67
PannnTastic <pannntastic@gmail.com>
2024-03-19 17:40:51 +0700

    Contoh Perulangan For

---
+++ PertemuanKetigaLooping.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    string nama [5];
+
+    for (i = 150; i > 100; i-=10){
+
+        cout<< i <<". "<<"SELAMAT PUASA" << endl;
+    }
+
+    cout<<"Nilai i Terakhir = "<< i << endl;
+
+    for(i = 0;i < 5;i++){
+        cout<<"Masukkan Nama ke- "<< i << ":";
+        cin>>nama[i];
+    }
+
+    for(i = 0;i < 5;i++){
+        cout<<"Nama Ke- "<< i <<". "<<nama[i] <<endl;
+    }
+}
